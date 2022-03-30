065c31f9459d90386bc17a98bfa4bd292ee99b9a
Carlos Nava <machinery.e@gmail.com>
2022-03-30 15:16:24 -0400

    Separar esquemas del template

--- src/app/schema-form/components/basic-data-only-mode/basic-data-only-mode.component.ts
+++ src/app/schema-form/components/basic-data-only-mode/basic-data-only-mode.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonObject } from './data';
 
 @Component({
   selector: 'app-basic-data-only-mode',
@@ -6,35 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-data-only-mode.component.scss']
 })
 export class BasicDataOnlyModeComponent {
-  exampleJsonObject = {
-    "first_name": "Jane",
-    "last_name": "Doe",
-    "age": 25,
-    "is_company": false,
-    "address": {
-      "street_1": "123 Main St.",
-      "street_2": null,
-      "city": "Las Vegas",
-      "state": "NV",
-      "zip_code": "89123"
-    },
-    "phone_numbers": [
-      {
-        "number": "702-123-4567",
-        "type": "cell"
-      },
-      {
-        "number": "702-987-6543",
-        "type": "work"
-      }
-    ],
-    "test_fields":[
-      {
-        "decimal": "0",
-        "type_number": "decimal"
-      }
-    ],
-    "notes": "",
-  };
+  /**
+   * Objeto Json
+   */
+  exampleJsonObject = JsonObject;
+  /**
+   * Constructor del documento
+   */
   constructor() { }
 }
---
+++ src/app/schema-form/components/basic-data-only-mode/data/json-object.ts
@@ -0,0 +1,30 @@
+export const JsonObject = {
+  "first_name": "Jane",
+  "last_name": "Doe",
+  "age": 25,
+  "is_company": false,
+  "address": {
+    "street_1": "123 Main St.",
+    "street_2": null,
+    "city": "Las Vegas",
+    "state": "NV",
+    "zip_code": "89123"
+  },
+  "phone_numbers": [
+    {
+      "number": "702-123-4567",
+      "type": "cell"
+    },
+    {
+      "number": "702-987-6543",
+      "type": "work"
+    }
+  ],
+  "test_fields":[
+    {
+      "decimal": "0",
+      "type_number": "decimal"
+    }
+  ],
+  "notes": "",
+};
